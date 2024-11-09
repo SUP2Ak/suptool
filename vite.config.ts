126ba7a35ccd173fb49440ac46e5b25e60cd29be
SUP2Ak <cormier.wesley@gmail.com>
2024-11-09 02:36:18 +0100

    ♻️ refactor(config): update Vite configuration structure
    - Added an async function call in the Vite config for proper initialization
    - Improved readability by reorganizing the structure of the exported function

--- vite.config.ts
+++ vite.config.ts
@@ -9,28 +9,32 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vitejs.dev/config/
 
 // @ts-ignore: eslint-disable-next-line require-await
-export default defineConfig(async () => ({
-  plugins: [react()],
+export default defineConfig(async () => {
+  // @ts-ignore: eslint-disable-next-line require-await
+  await asyncFunction(); // Ajoutez l'appel à votre fonction asynchrone ici
+  return {
+    plugins: [react()],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-        protocol: "ws",
-        host,
-        port: 1421,
-      }
-      : undefined,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+    //
+    // 1. prevent vite from obscuring rust errors
+    clearScreen: false,
+    // 2. tauri expects a fixed port, fail if that port is not available
+    server: {
+      port: 1420,
+      strictPort: true,
+      host: host || false,
+      hmr: host
+        ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+        : undefined,
+      watch: {
+        // 3. tell vite to ignore watching `src-tauri`
+        ignored: ["**/src-tauri/**"],
+      },
     },
-  },
-}));
+  };
+});
